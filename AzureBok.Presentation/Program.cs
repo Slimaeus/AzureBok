5c5992a321d0a3ba27ac362f87d8296e51b51b6d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-13 09:31:09 +0700

    Add project files.

---
+++ AzureBok.Presentation/Program.cs
@@ -0,0 +1,61 @@
+using AzureBok.Presentation.Settings;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration
+    .AddAzureAppConfiguration(options =>
+    {
+        options.Connect(builder.Configuration.GetConnectionString("AzureAppConfiguration"));
+        options
+            .Select(KeyFilter.Any, LabelFilter.Null)
+            .Select(KeyFilter.Any, "Test");
+    });
+
+builder.Services.Configure<AzureSettings>(builder.Configuration.GetSection(""));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("name", (IOptions<AzureSettings> settings) => settings.Value.Name);
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ AzureBok.Presentation/Settings/AzureSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace AzureBok.Presentation.Settings;
+
+public sealed class AzureSettings
+{
+    public string Name { get; set; } = string.Empty;
+}
